b64216345dede82fa0d82d2f0c7e0fea46e8f5ef
Evelina Voronina <118778262+Evelina-Vega@users.noreply.github.com>
2022-12-06 11:47:10 -0500

    parse out cvs

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gocarina/gocsv"
+	"os"
+	"strings"
+)
+
+type AWS struct {
+	Status   string `csv:"status_code"`
+	Method   string `csv:"http_method"`
+	Uri      string `csv:"absolute_uri"`
+	Relative string
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	in, err := os.Open("api.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer in.Close()
+
+	clients := []*AWS{}
+
+	if err := gocsv.UnmarshalFile(in, &clients); err != nil {
+		panic(err)
+	}
+
+	var store = map[string]int{}
+
+	for _, client := range clients {
+		relative := strings.Split(client.Uri, "?")
+		client.Relative = relative[0]
+		fmt.Printf("%+v\n", client.Relative)
+		store[client.Relative] += 1
+	}
+
+	fmt.Printf("%v\n", store)
+
 }
